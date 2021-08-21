c6190e487f2310ab58c04d06074e583d4dc1dab7
bypanzer <89009367+bypanzer@users.noreply.github.com>
2021-08-21 22:23:45 +0400

    Update wallpaper.py

--- SaitamaRobot/modules/wallpaper.py
+++ SaitamaRobot/modules/wallpaper.py
@@ -24,7 +24,7 @@ def wall(update: Update, context: CallbackContext):
         caption = query
         term = query.replace(" ", "%20")
         json_rep = r.get(
-            f"https://wall.alphacoders.com/api2.0/get.php?method=highest_rated&page=1&info_level=2&page=2}"
+            f"https://wall.alphacoders.com/api2.0/get.php?auth={WALL_API}&method=search&term={term}"
         ).json()
         if not json_rep.get("success"):
             msg.reply_text(f"Bir problem yarandı! Daha sonra cəhd edin.")
